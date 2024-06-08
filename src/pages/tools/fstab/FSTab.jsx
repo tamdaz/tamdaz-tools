5a22c775639e46249088843246348f9211cc8903
tamdaz <tamda.zohir.pro@gmail.com>
2024-06-09 00:35:28 +0200

    fstab: Update names for variables, states and functions.

--- src/pages/tools/fstab/FSTab.jsx
+++ src/pages/tools/fstab/FSTab.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import FSTabForm from "./FSTabForm";
 import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
 import { monokai } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { renderToStaticMarkup, renderToString } from "react-dom/server";
 
 import Alert from "../../../components/Alert";
 
@@ -9,125 +10,172 @@ import Alert from "../../../components/Alert";
  * FSTab page
  */
 function FSTab() {
-    const [id, setId] = React.useState(1);
+	const [id, setId] = React.useState(1);
+	const [tabSize, setTabSize] = React.useState(8);
 
-    const [tabs, setTabs] = React.useState([{
-        id: id,
-        device: "/dev/sda1",
-        mountPoint: "/",
-        fileSystem: "ext4",
-        options: "defaults",
-        dump: 0,
-        verify: 0
-    }]);
+	const [rows, setRows] = React.useState([
+		{
+			id: id,
+			device: "/dev/sda1",
+			mountPoint: "/",
+			fileSystem: "ext4",
+			options: "defaults",
+			dump: 0,
+			verify: 0,
+		},
+	]);
 
-    document.title = "Générateur du fichier fstab"
+	document.title = "Générateur du fichier fstab";
 
-    useEffect(() => {
-        setId(id + 1);
-        
-        setTabs(tabs);
-    }, [tabs]);
+	useEffect(() => {
+		setId(id + 1);
+		setRows(rows);
+	}, [rows]);
 
-    /**
-     * Create the new tab.
-     */
-    const createTab = () => {
-        setTabs([
-            ...tabs, {
-                id: id,
-                device: "",
-                mountPoint: "",
-                fileSystem: "ext4",
-                options: "",
-                dump: 1,
-                verify: 2
-            }
-        ]);
-    }
+	/**
+	 * Display the result of FSTab file.
+	 */
+	const FSTabOutput = () => {
+		let output = "";
 
-    /**
-     * Update the next row tab in tabs state.
-     * 
-     * @param {Array} nextTab
-     */
-    const updateTab = (nextTab) => {
-        const updatedTabs = [...tabs.filter(tab => tab.id !== nextTab.id), nextTab];
+		rows.map(
+			({ device, mountPoint, fileSystem, options, dump, verify }) => {
+				output += device;
+				output += "\t";
+				output += mountPoint;
+				output += "\t";
+				output += fileSystem;
+				output += "\t";
+				output += options;
+				output += "\t";
+				output += dump ? 1 : 0;
+				output += "\t";
+				output += verify;
+				output += "\n";
+			},
+		);
 
-        updatedTabs.sort((a, b) => a.id - b.id);
+		return output;
+	};
 
-        setTabs(updatedTabs);
-    }
+	/**
+	 * Create the new row.
+	 */
+	const createRow = () => {
+		setRows([
+			...rows,
+			{
+				id: id,
+				device: "",
+				mountPoint: "",
+				fileSystem: "ext4",
+				options: "",
+				dump: 1,
+				verify: 2,
+			},
+		]);
+	};
 
-    /**
-     * Delete the selected row tab.
-     */
-    const deleteTab = (tabToDelete) => {
-        const newTabs = [...tabs.filter(tab => tab.id !== tabToDelete.id)];
+	/**
+	 * Update the next row in tabs state.
+	 *
+	 * @param {{
+	 *     id: number,
+	 *     device: string,
+	 *     mountPoint: string,
+	 *     fileSystem: string,
+	 *     options: string,
+	 *     dump: number,
+	 *     verify: number
+	 * }} nextRow
+	 */
+	const updateRow = (nextRow) => {
+		const updatedRows = [
+			...rows.filter((row) => row.id !== nextRow.id),
+			nextRow,
+		];
+		updatedRows.sort((a, b) => a.id - b.id);
+		setRows(updatedRows);
+	};
 
-        setTabs(newTabs);
-    }
+	/**
+	 * Delete the selected row.
+	 *
+	 * @param {{
+	 *     id: number,
+	 *     device: string,
+	 *     mountPoint: string,
+	 *     fileSystem: string,
+	 *     options: string,
+	 *     dump: number,
+	 *     verify: number
+	 * }} rowToDelete
+	 */
+	const deleteRow = (rowToDelete) => {
+		const newRows = [...rows.filter((row) => row.id !== rowToDelete.id)];
 
-    /**
-     * Copy the result fstab output.
-     * 
-     * @param {Event & { target: HTMLDivElement }} e 
-     */
-    const copyOutput = (e) => {
-        navigator.clipboard.writeText(e.target.textContent);
-    }
+		setRows(newRows);
+	};
 
-    return <>
-        <h1>Générateur FSTab</h1>
+	/**
+	 * Copy the result FSTab output.
+	 *
+	 * @param {Event & { target: HTMLDivElement }} e
+	 */
+	const copyOutput = (e) => {
+		navigator.clipboard.writeText(e.target.textContent);
+	};
 
-        <Alert
-            type="warning"
-            title="Avertissement sur la configuration de fstab."
-            description="Avant de faire des modifications dans le fichier /etc/fstab, assurez-vous de garder l'ancienne version de ce fichier pour éviter les erreurs."
-        />
-
-        <div className="grid grid-cols-7 gap-8 text-center py-4">
-            <span>Périphérique</span>
-            <span>Point de montage</span>
-            <span>Système de fichiers</span>
-            <span>Options</span>
-            <span>Sauvegarder ?</span>
-            <span>Vérifier ?</span>
-            <span>Actions</span>
-        </div>
-
-        {
-            tabs.length !== 0 ? tabs.map((tab) => {
-                return <FSTabForm
-                    id={tab.id}
-                    updateTab={updateTab}
-                    deleteTab={deleteTab}
-                    key={`fstab-form-${tab.id}`}
-                />
-            }) : ""
-        }
-
-        <button className="px-4 py-2 bg-green-500 hover:bg-green-600" onClick={createTab}>
-            Créer une ligne
-        </button>
-
-        <br />
-        <br />
-        <h2>Résultat : </h2>
-        
-        <div style={{ tabSize: 25 }}>
-            <SyntaxHighlighter language="bash" style={monokai} onClick={copyOutput}>
-                {
-                    tabs.length !== 0 ? 
-                    "#<file system>\t<dir>\t<type>\t<options\t<dump>\t<pass>\n" +
-                    tabs.map(({ device, mountPoint, fileSystem, options, dump, verify }) => {
-                        return `${device}\t${mountPoint}\t${fileSystem}\t${options}\t${dump ? 1 : 0}\t${verify}`
-                    }).join("\n") : " "
-                }
-            </SyntaxHighlighter>
-        </div>
-        <Alert description="Vous pouvez cliquer dessus pour copier ce résultat" />
-    </>
+	return (
+		<>
+			<h1>Générateur FSTab</h1>
+			<Alert
+				type="warning"
+				title="Avertissement sur la configuration de fstab."
+				description="Avant de faire des modifications dans le fichier /etc/fstab, assurez-vous de garder l'ancienne version de ce fichier pour éviter les erreurs."
+			/>
+			<div className="grid grid-cols-7 gap-8 text-center py-4">
+				<span>Périphérique</span>
+				<span>Point de montage</span>
+				<span>Système de fichiers</span>
+				<span>Options</span>
+				<span>Sauvegarder ?</span>
+				<span>Vérifier ?</span>
+				<span>Actions</span>
+			</div>
+			{rows.length !== 0
+				? rows.map((row) => {
+						return (
+							<FSTabForm
+								id={row.id}
+								updateRow={updateRow}
+								deleteRow={deleteRow}
+								key={`fstab-form-${row.id}`}
+							/>
+						);
+					})
+				: ""}
+			<button
+				className="px-4 py-2 bg-green-500 hover:bg-green-600"
+				onClick={createRow}
+			>
+				Créer une ligne
+			</button>
+			<br />
+			<br />
+			<h2>Résultat : </h2>
+			<div style={{ tabSize: 8 }}>
+				<SyntaxHighlighter
+					language="bash"
+					style={monokai}
+					onClick={copyOutput}
+				>
+					{renderToString(<FSTabOutput />)}
+				</SyntaxHighlighter>
+			</div>
+			<Alert description="Vous pouvez cliquer copier ce résultat en cliquant dessus." />
+		</>
+	);
 }
 
 export default FSTab;
--- src/pages/tools/fstab/FSTabForm.jsx
+++ src/pages/tools/fstab/FSTabForm.jsx
@@ -2,70 +2,119 @@ import React, { useEffect } from "react";
 
 /**
  * Allows to select filesystem
- * 
+ *
  * @param {Object} obj
  * @param {string} obj.name
  * @param {string} obj.value
  * @param {string} obj.onChange
  */
 function FileSystemSelect({ name, value, onChange }) {
-    const filesystems = [
-        "btrfs", "vfat", "ext2", "ext3", "ext4",
-        "hfs", "jfs", "ntfs", "reiserfs", "udf", "xfs", "zfs"
-    ]
+	const filesystems = [
+		"btrfs",
+		"vfat",
+		"ext2",
+		"ext3",
+		"ext4",
+		"hfs",
+		"jfs",
+		"ntfs",
+		"reiserfs",
+		"udf",
+		"xfs",
+		"zfs",
+	];
 
-    return <select name={name} onChange={onChange} value={value}>
-        { filesystems.map(v => <option value={ v } key={v}>{ v }</option>) }
-    </select>
+	return (
+		<select name={name} onChange={onChange} value={value}>
+			{filesystems.map((v) => (
+				<option value={v} key={v}>
+					{v}
+				</option>
+			))}
+		</select>
+	);
 }
 
 /**
  * @param {Object} obj
  * @param {number} obj.id
- * @param {Function} updateTab
- * @param {Function} deleteTab
+ * @param {Function} updateRow
+ * @param {Function} deleteRow
  */
-function FSTabForm({ id, updateTab, deleteTab }) {
-    const [tab, setTab] = React.useState({
-        id: id,
-        device: "/dev/sda1",
-        mountPoint: "/",
-        fileSystem: "ext4",
-        options: "defaults",
-        dump: 0,
-        verify: 0
-    });
+function FSTabForm({ id, updateRow, deleteRow }) {
+	const [row, setRow] = React.useState({
+		id: id,
+		device: "/dev/sda1",
+		mountPoint: "/",
+		fileSystem: "ext4",
+		options: "defaults",
+		dump: 0,
+		verify: 0,
+	});
 
-    useEffect(() => {
-        updateTab(tab);
-    }, [tab]);
+	useEffect(() => {
+		updateRow(row);
+	}, [row]);
 
-    /**
-     * @param {Event & { target: HTMLInputElement }} e 
-     */
-    const handleUpdate = (e) => {
-        const name = e.target.name;
-        const value = e.target.type === "checkbox" ? e.target.checked : e.target.value;
+	/**
+	 * @param {Event & { target: HTMLInputElement }} e
+	 */
+	const handleUpdate = (e) => {
+		const name = e.target.name;
+		const value =
+			e.target.type === "checkbox" ? e.target.checked : e.target.value;
 
-        setTab({
-            ...tab,
-            [name]: value
-        })
-    }
+		setRow({ ...row, [name]: value });
+	};
 
-    return <div className="grid grid-cols-7 gap-4 pb-4">
-        <input onChange={handleUpdate} name="device" type="text" placeholder="/dev/sda1" value={tab.device} />
-        <input onChange={handleUpdate} name="mountPoint" type="text" placeholder="/" value={tab.mountPoint} />
-        <FileSystemSelect name="fileSystem" onChange={handleUpdate} value={tab.fileSystem} />
-        <input name="options" onChange={handleUpdate} type="text" placeholder="defaults" value={tab.options} />
-        <input name="dump" onChange={handleUpdate} type="checkbox" className="scale-50" value={tab.dump} />
-        <select name="verify" onChange={handleUpdate} value={tab.verify}>
-            <option value="0">Ignorer</option>
-            <option value="1">Vérifier</option>
-            <option value="2">Vérifier après</option>
-        </select>
-        <button className="bg-red-500 text-white" onClick={() => deleteTab(tab)}>Supprimer</button>
-    </div>
+	return (
+		<div className="grid grid-cols-7 gap-4 pb-4">
+			<input
+				onChange={handleUpdate}
+				name="device"
+				type="text"
+				placeholder="/dev/sda1"
+				value={row.device}
+			/>
+			<input
+				onChange={handleUpdate}
+				name="mountPoint"
+				type="text"
+				placeholder="/"
+				value={row.mountPoint}
+			/>
+			<FileSystemSelect
+				name="fileSystem"
+				onChange={handleUpdate}
+				value={row.fileSystem}
+			/>
+			<input
+				name="options"
+				onChange={handleUpdate}
+				type="text"
+				placeholder="defaults"
+				value={row.options}
+			/>
+			<input
+				name="dump"
+				onChange={handleUpdate}
+				type="checkbox"
+				className="scale-50"
+				value={row.dump}
+			/>
+			<select name="verify" onChange={handleUpdate} value={row.verify}>
+				<option value="0">Ignorer</option>
+				<option value="1">Vérifier</option>
+				<option value="2">Vérifier après</option>
+			</select>
+			<button
+				className="bg-red-500 text-white"
+				onClick={() => deleteRow(row)}
+			>
+				Supprimer
+			</button>
+		</div>
+	);
 }
 
 export default FSTabForm;
