237eed21aab971657a21297f207e4dae04eef002
tamdaz <tamda.zohir.pro@gmail.com>
2024-06-09 01:22:13 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 export default function Home() {
 	document.title = "Accueil";
-	return (
-		<>
-			<h1>Hello world</h1>
-		</>
-	);
+	return <>
+		<h1>Bienvenue sur tools.tamdaz.fr.</h1>
+		<p>
+			Sur ce site, vous trouverez (bientôt) des pages qui permettront de simplifier la configuration des
+			différents outils.
+		</p>
+	</>
 }
